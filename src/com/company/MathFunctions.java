d99cd41f1c7aeca5328094278da60da2c611e3f1
rlima <rlima@atlassian.com>
2021-05-10 00:34:11 +1000

    easy

--- src/com/company/MathFunctions.java
+++ src/com/company/MathFunctions.java
@@ -1,14 +1,19 @@
 package com.company;
 
-public class Main {
+public class MathFunctions {
     public static void addToInt(int x, int amountToAdd) {
         x = x + amountToAdd;
     }
     public static void main(String[] args) {
 	    var a = 15;
 	    var b = 10;
-	    addToInt(a, b);
+        MathFunctions.addToInt(a, b);
         System.out.println(a);
     }
 }
+
+/*
+Q: 10, 15, compile error line5, compile errpr line 8, none?
+A: 15
+ */
 
