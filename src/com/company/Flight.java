4ac53b66e29d896772466b1232d5a16daf12f6d2
rlima <rlima@atlassian.com>
2021-05-10 01:39:54 +1000

    public class

---
+++ src/com/company/Flight.java
@@ -0,0 +1,22 @@
+package com.company;
+/*
+public class Flight {
+    private FlightNumber number;
+
+    public Flight(FlightNumber number) {
+        this.number = number;
+    }
+}
+public class FlightNumber { //compile error: Class 'FlightNumber' is public, should be declared in a file named 'FlightNumber.java' or make it not public
+    public int value;
+    public String code;
+}
+*/
+/*
+QUESTION:
+The code compiles successfully and two bytecode files are generated: Flight.class and FlightNumber.class.
+The code compiles successfully and one bytecode file is generated: Flight.class.
+A compiler error occurs on line 4.
+A compiler error occurs on line 6.
+A compiler error occurs on line 10.
+ */
