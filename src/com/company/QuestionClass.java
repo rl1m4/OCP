95c46121db9989b364cf6c74e06270c4c616cfb3
rlima <rlima@atlassian.com>
2021-05-19 20:15:23 +1000

    Members of a class

---
+++ src/com/company/QuestionClass.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class QuestionClass {
+    void var() {}
+}
+
+/*
+Which of the following are valid instance members of a class? (Choose all that apply.) A. var var = 3;
+B. Var case = new Var();
+C. void var() {}
+D. int Var() { var _ = 7; return _;} E. String new = "var";
+F. var var() { return null; }
+
+A: Only C
+ */
