84a7dfceb939081b39023b2647198499a7f9930b
rlima <rlima@atlassian.com>
2021-05-19 20:12:32 +1000

    Run modular program

---
+++ src/com/company/RunModularProgram.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class RunModularProgram {
+}
+
+/*
+Which of the following will run a modular program? A. java -cp modules mod/class
+B. java -cp modules -m mod/class
+C. java -cp modules -p mod/class
+D. java -m modules mod/class
+E. java -m modules -p mod/class F. java -p modules mod/class
+G. java -p modules -m mod/class
+
+A: G: The new -p option specifies the module path.
+The new -m option precedes the program to be run in the format moduleName/fullyQualifiedClassName.
+ */
